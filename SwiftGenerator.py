046659be5e5415ed71c31a6478b6ff41701912b7
Jim Oxenford <joxenford@me.com>
2015-10-12 14:31:56 -0400

    refactor UI classes out of generator

--- SwiftGenerator.py
+++ SwiftGenerator.py
@@ -1,40 +1,4 @@
-import sys, string
-
-class Label:
-    def __init__(self, name):
-        self.name = name
-        self.textColor = None
-        self.font = None
-        
-    def setTextColor(self, textColor):
-        self.textColor = textColor
-
-class Button:
-    def __init__(self, name):
-        self.name = name
-        self.tintColor = None
-
-    def setTintColor(self, tintColor):
-        self.tintColor = tintColor
-
-class Font:
-    def __init__(self, name, size):
-        self.name = name
-        self.size = size
-
-    def toSwift(self):
-        return "UIFont (name: \"%s\", size: %s)" % (self.name, self.size)
-
-class Color:
-    
-    def __init__(self, red, green, blue, alpha):
-        self.red = red
-        self.green = green
-        self.blue = blue
-        self.alpha = alpha
-        
-    def toSwift(self):
-        return "UIColor(red: %.2f, green: %.2f, blue: %.2f, alpha: %.2f)" % (self.red, self.green, self.blue, self.alpha)
+import sys, string, PyUI
 
 class SwiftGenerator:
     
@@ -54,27 +18,29 @@ class SwiftGenerator:
     def newline(self):
         self.write("\n")
 
-    def labelOutletCollection(self, label):
-        self.write(self.iboutlet + label.name + ": "+ self.labelArray + "{" )
-        self.indent()
-        self.newline()
-        self.didSet(label.name)
-        self.newline()
-        self.outdent()
-        self.write("}")
-        self.newline()
-        self.newline()
+    def labelOutletCollections(self, labels = []):
+        for label in labels:
+            self.write(self.iboutlet + label.name + ": "+ self.labelArray + "{" )
+            self.indent()
+            self.newline()
+            self.didSet(label.name)
+            self.newline()
+            self.outdent()
+            self.write("}")
+            self.newline()
+            self.newline()
 
-    def buttonOutletCollection(self, button):
-        self.write(self.iboutlet + button.name + ": "+ self.buttonArray + "{" )
-        self.indent()
-        self.newline()
-        self.didSet(button.name)
-        self.newline()
-        self.outdent()
-        self.write("}")
-        self.newline()
-        self.newline()
+    def buttonOutletCollections(self, buttons = []):
+        for button in buttons:
+            self.write(self.iboutlet + button.name + ": "+ self.buttonArray + "{" )
+            self.indent()
+            self.newline()
+            self.didSet(button.name)
+            self.newline()
+            self.outdent()
+            self.write("}")
+            self.newline()
+            self.newline()
         
     def didSet(self, name):
         self.write("didSet {")
@@ -85,27 +51,34 @@ class SwiftGenerator:
         self.outdent()
         self.write("}")
        
-    def buildLabelStyleFunction(self, label):
-        self.write("func " + "style" + label.name + self.labelArgument + " {" )
-        self.newline()
-        self.indent()
-        self.write("for object in " + label.name + " {")
-        self.newline()
-        self.indent()
-        if label.font: set([self.write("object.font = " + label.font.toSwift())]),  self.newline()
-        if label.textColor: set([self.write("object.textColor = " + label.textColor)]), self.newline()
-        self.indent()
-        self.write("}")
-        self.newline()
-        self.outdent()
-        self.outdent()
-        self.write("}")
+    def buildLabelStyleFunctions(self, labels = []):
+        for label in labels:
+            self.write("func " + "style" + label.name + self.labelArgument + " {" )
+            self.newline()
+            self.indent()
+            self.write("for object in " + label.name + " {")
+            self.newline()
+            self.indent()
+            if label.font: set([self.write("object.font = " + label.font.toSwift())]),  self.newline()
+            if label.textColor: set([self.write("object.textColor = " + label.textColor)]), self.newline()
+            self.write("}")
+            self.newline()
+            self.outdent()
+            self.write("}")
+            self.nextFunction()
 
-    def buildButtonStyleFunction(self, button):
-        self.write("func " + "style" + button.name + self.buttonArgument + " {" )
-        self.newline()
-        self.indent()
-        self.write("}")
+
+
+    def buildButtonStyleFunctions(self, buttons = []):
+        for button in buttons:
+            self.write("func " + "style" + button.name + self.buttonArgument + " {" )
+            self.newline()
+            self.indent()
+            self.write("for object in " + button.name + " {")
+            self.newline()
+            self.indent()
+            if button.backgroundColor: set([self.write("object.backgroundColor = " + button.backgroundColor)]), self.newline()
+
 
  
     def write(self, string):
@@ -119,8 +92,9 @@ class SwiftGenerator:
             raise SyntaxError, "internal error in code generator"
         self.level = self.level - 1
 
-    def newFunction(self):
-        self.outdent()
+    def nextFunction(self):
+        self.level = 0
+        ##self.outdent()
         self.newline()
         self.newline()
 
@@ -142,40 +116,43 @@ class SwiftGenerator:
 
 #################################################
 swiftGenerator = SwiftGenerator()
+pyUI = PyUI
 file = open("Theme.swift", "w")
 
 ## define h1 ##
-H1Font = Font("Asul", 34)
-H1Color = Color(1, 0, 0, 1)
-H1Label = Label("H1Label")
+H1Font = pyUI.Font("Asul", 34)
+H1Color = pyUI.Color(1, 0, 0, 1)
+H1Label = pyUI.Label("H1Label")
 H1Label.font = H1Font
 H1Label.setTextColor(H1Color.toSwift())
 
 ## define h2 ##
-H2font = Font("Asul", 24)
-H2Color = Color(0, 1, 0, 1)
-H2Label = Label("H2Label")
+H2font = pyUI.Font("Asul", 24)
+H2Color = pyUI.Color(0, 1, 0, 1)
+H2Label = pyUI.Label("H2Label")
 H2Label.font = H2font
 H2Label.setTextColor(H2Color.toSwift())
 
 ## define button1 ##
-Button1 = Button("Button1")
-Button1Color = Color(0, 0, 1, 1)
-Button1.tintColor = Button1Color
+Button1 = pyUI.Button("Button1")
+Button1Color = pyUI.Color(0, 0, 1, 1)
+Button1.setBackgroundColor(Button1Color.toSwift())
 
+##define button2 ##
+Button2 = pyUI.Button("Button2")
+Button2Color = pyUI.Color(1, 1, 0, 1)
+Button2.setBackgroundColor(Button2Color.toSwift())
 
 ## begin swift generation ##
 swiftGenerator.begin(tab="    ")
 swiftGenerator.openClass()
-swiftGenerator.labelOutletCollection(H1Label)
-swiftGenerator.labelOutletCollection(H2Label)
-swiftGenerator.buttonOutletCollection(Button1)
-swiftGenerator.buildLabelStyleFunction(H1Label)
-swiftGenerator.newFunction()
-swiftGenerator.buildButtonStyleFunction(Button1)
-swiftGenerator.newFunction()
-swiftGenerator.buildLabelStyleFunction(H2Label)
+swiftGenerator.labelOutletCollections([H1Label, H2Label])
+swiftGenerator.buttonOutletCollections([Button1, Button2])
+swiftGenerator.buildLabelStyleFunctions([H1Label, H2Label])
+swiftGenerator.nextFunction()
+swiftGenerator.buildButtonStyleFunctions([Button1, Button2])
 swiftGenerator.closeClass()
+
 file.write(swiftGenerator.end())
 file.close()
 print swiftGenerator.end()
---
+++ Theme.swift
@@ -0,0 +1,52 @@
+import UIKit
+
+class Theme: NSObject {
+
+    @IBOutlet var H1Label: [UILabel]! {        
+        didSet {            
+            styleH1Label(H1Label)            
+        }        
+    }    
+    
+    @IBOutlet var H2Label: [UILabel]! {        
+        didSet {            
+            styleH2Label(H2Label)            
+        }        
+    }    
+    
+    @IBOutlet var Button1: [UIButton]! {        
+        didSet {            
+            styleButton1(Button1)            
+        }        
+    }    
+    
+    @IBOutlet var Button2: [UIButton]! {        
+        didSet {            
+            styleButton2(Button2)            
+        }        
+    }    
+    
+    func styleH1Label(labels: [UILabel]) {    
+        for object in H1Label {        
+            object.font = UIFont (name: "Asul", size: 34)            
+            object.textColor = UIColor(red: 1.00, green: 0.00, blue: 0.00, alpha: 1.00)            
+            }            
+        }
+
+func styleH2Label(labels: [UILabel]) {
+    for object in H2Label {    
+        object.font = UIFont (name: "Asul", size: 24)        
+        object.textColor = UIColor(red: 0.00, green: 1.00, blue: 0.00, alpha: 1.00)        
+        }        
+    }
+
+
+
+func styleButton1(buttons: [UIButton]) {
+    for object in Button1 {    
+        object.backgroundColor = UIColor(red: 0.00, green: 0.00, blue: 1.00, alpha: 1.00)        
+        func styleButton2(buttons: [UIButton]) {        
+            for object in Button2 {            
+                object.backgroundColor = UIColor(red: 1.00, green: 1.00, blue: 0.00, alpha: 1.00)                
+            
+        }
