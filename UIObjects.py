39edc033d7707bb695c9c2e848209656277cf0af
Jim Oxenford <joxenford@me.com>
2015-10-21 16:26:15 -0400

    added hex to rgb

--- UIObjects.py
+++ UIObjects.py
@@ -94,3 +94,8 @@ class Color:
 
     def rgb_to_hex(self, r,g,b):
         return "#%02X%02X%02X" % (r,g,b)
+
+    def hex_to_rgb(value):
+        value = value.lstrip('#')
+        lv = len(value)
+        return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
