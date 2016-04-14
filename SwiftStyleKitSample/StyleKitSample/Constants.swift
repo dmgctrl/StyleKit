f068b4d2a112247655147bb347ae20744ce70a4e
Van Nguyen <vannguyen@Vans-MacBook-Pro.local>
2016-04-14 10:59:49 -0400

    Remove enum failable initializers.

--- SwiftStyleKitSample/StyleKitSample/Constants.swift
+++ SwiftStyleKitSample/StyleKitSample/Constants.swift
@@ -7,15 +7,6 @@ enum FontProperties: String {
 
 enum LabelProperties: String {
     case FontStyle = "fontStyle"
-    
-    init?(rawValue: String) {
-        switch rawValue {
-            case "fontStyle":
-                self = .FontStyle
-            default:
-                return nil
-        }
-    }
 }
 
 enum ButtonProperties: String {
@@ -24,23 +15,6 @@ enum ButtonProperties: String {
     case BorderColor = "borderColor"
     case CornerRadius = "cornerRadius"
     case Normal = "normal"
-    
-    init?(rawValue: String) {
-        switch rawValue {
-            case "fontStyle":
-                self = .FontStyle
-            case "borderWidth":
-                self = .BorderWidth
-            case "borderColor":
-                self = .BorderColor
-            case "cornerRadius":
-                self = .CornerRadius
-            case "normal":
-                self = .Normal
-            default:
-                return nil
-        }
-    }
 }
 
 enum TextFieldProperties: String {
@@ -51,27 +25,6 @@ enum TextFieldProperties: String {
     case TextAlignment = "textAlignment"
     case BorderStyle = "borderStyle"
     case TextColor = "textColor"
-    
-    init?(rawValue: String) {
-        switch rawValue {
-            case "fontStyle":
-                self = .FontStyle
-            case "borderWidth":
-                self = .BorderWidth
-            case "borderColor":
-                self = .BorderColor
-            case "cornerRadius":
-                self = .CornerRadius
-            case "borderStyle":
-                self = .BorderStyle
-            case "textColor":
-                self = .TextColor
-            case "textAlignment":
-                self = .TextAlignment
-            default:
-                return nil
-        }
-    }
 }
 
 enum SegmentedControlProperties: String {
@@ -79,21 +32,6 @@ enum SegmentedControlProperties: String {
     case NormalState = "normalState"
     case SelectedState = "selectedState"
     case TintColor = "tintColor"
-    
-    init?(rawValue: String) {
-        switch rawValue {
-            case "fontStyle":
-                self = .FontStyle
-            case "normalState":
-                self = .NormalState
-            case "selectedState":
-                self = .SelectedState
-            case "tintColor":
-                self = .TintColor
-            default:
-                return nil
-        }
-    }
 }
 
 enum SliderProperties: String {
@@ -102,23 +40,6 @@ enum SliderProperties: String {
     case ThumbImage = "thumbImage"
     case FilledTrackImage = "filledTrackImage"
     case EmptyTrackImage = "emptyTrackImage"
-    
-    init?(rawValue: String) {
-        switch rawValue {
-            case "filledTrackColor":
-                self = .FilledTrackColor
-            case "emptyTrackColor":
-                self = .EmptyTrackColor
-            case "thumbImage":
-                self = .ThumbImage
-            case "filledTrackImage":
-                self = .FilledTrackImage
-            case "emptyTrackImage":
-                self = .EmptyTrackImage
-            default:
-                return nil
-        }
-    }
 }
 
 enum ColorProperties: String {
