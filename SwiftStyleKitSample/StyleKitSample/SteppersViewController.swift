dd3eaec58f63ef93842fb6790a210e2fd67a8e87
Van Nguyen <vannguyen@Vans-MacBook-Pro.local>
2016-04-19 09:51:28 -0400

    Add labels to show steppers' values.

--- SwiftStyleKitSample/StyleKitSample/SteppersViewController.swift
+++ SwiftStyleKitSample/StyleKitSample/SteppersViewController.swift
@@ -14,10 +14,17 @@ class SteppersViewController: UIViewController {
         }
     }
     
+    @IBOutlet weak var stepper1: UIStepper!
+    @IBOutlet weak var stepper2: UIStepper!
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.label1.text = "\(stepper1.value)"
+        self.label2.text = "\(stepper2.value)"
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,4 +32,12 @@ class SteppersViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func stepperOneValueChanged(sender: UIStepper) {
+        self.label1.text = "\(stepper1.value)"
+    }
+    
+    @IBAction func stepperTwoValueChanged(sender: UIStepper) {
+        self.label2.text = "\(stepper2.value)"
+    }
+    
 }
