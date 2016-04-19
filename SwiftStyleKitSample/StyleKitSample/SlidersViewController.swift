12e3ed86030b2c57f09642037cba83474e3b7967
Van Nguyen <vannguyen@Vans-MacBook-Pro.local>
2016-04-19 09:41:18 -0400

    Assign maximum and minimum values to sliders.

--- SwiftStyleKitSample/StyleKitSample/SlidersViewController.swift
+++ SwiftStyleKitSample/StyleKitSample/SlidersViewController.swift
@@ -14,16 +14,37 @@ class SlidersViewController: UIViewController {
         }
     }
     
+    @IBOutlet weak var slider1: UISlider!
+    @IBOutlet weak var slider2: UISlider!
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.slider1.maximumValue = 100
+        self.slider1.minimumValue = 0
+        self.slider1.value = 0
         
+        self.slider2.maximumValue = 50
+        self.slider2.minimumValue = 0
+        self.slider2.value = 0
+        
+        self.label1.text = "\(self.slider1.value) / \(self.slider1.maximumValue)"
+        self.label2.text = "\(self.slider2.value) / \(self.slider2.maximumValue)"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func sliderOneValueChanged(sender: UISlider) {
+        self.label1.text = "\(self.slider1.value) / \(self.slider1.maximumValue)"
+    }
 
+    @IBAction func sliderTwoValueChanged(sender: UISlider) {
+        self.label2.text = "\(self.slider2.value) / \(self.slider2.maximumValue)"
+    }
 }
